90586192ab4f4eb2c23545de0857b8f3eb953daf
Shivam5278 <st527874@gmail.com>
2023-03-06 18:37:14 +0530

    Home page created

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import WebFont from 'webfontloader';
+import React from 'react';
+
+
+import Header from './component/layout/Header/Header.js';
+import Footer from './component/layout/Footer/Footer.js';
+import Home from './component/Home/Home.js';
+
+
+function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka'],
+      },
+    });
+  }, []);
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        
+      <Route exact path='/' element={<Home />}component = {Home} />
+      </Routes>
+
+
+
+    <Footer />
+    </Router>
+
+  );
+}
+
+export default App;
