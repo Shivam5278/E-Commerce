4c12abfc895002a152f1cb5eac83723c677b7af3
Shivam5278 <st527874@gmail.com>
2023-03-06 12:46:40 +0530

    Order Model, Routes and Controllers completed

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+const { newOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder } = require('../controllers/orderController');
+const { isAuthenticatedUser, authorizeRole } = require('../middleware/auth');
+
+
+router.route('/order/new').post(isAuthenticatedUser, newOrder);
+
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
+
+router.route('/orders/me').get(isAuthenticatedUser, myOrders );
+
+router.route('/admin/orders').get(isAuthenticatedUser,authorizeRole('admin'), getAllOrders );
+
+router
+    .route('/admin/order/:id')
+    .put(isAuthenticatedUser, authorizeRole('admin'), updateOrder )
+    .delete(isAuthenticatedUser, authorizeRole('admin'), deleteOrder );
+
+router.route('/orders/me').get(isAuthenticatedUser, myOrders );
+
+
+
+module.exports = router;
