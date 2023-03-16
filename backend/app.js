fe820748ca603662f4f958be623890d92ef3ce75
Shivam5278 <st527874@gmail.com>
2023-03-16 21:16:07 +0530

    Production test

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-//const path = require("path");
+const path = require("path");
 
 const errorMiddleware = require("./middleware/error");
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -34,11 +34,11 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 //Middleware for errors
 app.use(errorMiddleware);
